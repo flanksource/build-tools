8c13b62e7665d873992d406028319e6e68723111
Philip Stafford Wood <philip.wood@gmail.com>
2020-05-05 10:25:53 +0200

    cobra auto-generated CLI

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/flanksource/build-tools
+
+go 1.13
+
+require github.com/spf13/cobra v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2020 Flanksource
+This file is part of Flanksource build tools
+*/
+package main
+
+import "github.com/flanksource/build-tools/cmd"
+
+func main() {
+	cmd.Execute()
+}
