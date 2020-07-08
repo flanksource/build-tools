374d533e4d66ce61b23b0382a8f0e64ee5140c4b
Philip Stafford Wood <philip.wood@gmail.com>
2020-07-08 12:46:12 +0200

    add cmd to detect failed tests

--- cmd/junit.go
+++ cmd/junit.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/flanksource/build-tools/pkg/junit"
 	"github.com/spf13/cobra"
@@ -23,6 +24,25 @@ func init() {
 				return err
 			}
 			fmt.Println(results.GenerateMarkdown())
+
+			return nil
+		},
+	})
+	Junit.AddCommand(&cobra.Command{
+		Use:     "passfail",
+		Aliases: []string{"pf"},
+		Short: "Print result summary for JUnit test and set exit code to 1 if there are failed tests.",
+		Args:    cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			results, err := junit.ParseJunitResultFiles(args...)
+			if err != nil {
+				return err
+			}
+			fmt.Println(results.String())
+			if results.Failed > 0 {
+				os.Exit(1)
+			}
 
 			return nil
 		},
